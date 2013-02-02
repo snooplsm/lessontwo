5a515245cf96c94adf1c2007a9dad4a4d2ffaadf
Ryan Gravener <rgravener@wolf-matrix.local>
2013-02-02 11:16:16 -0500

    set a base adapter

--- src/us/wmwm/lessontwo/fragments/ListFragment.java
+++ src/us/wmwm/lessontwo/fragments/ListFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class ListFragment extends Fragment {
@@ -17,5 +18,43 @@ public class ListFragment extends Fragment {
 		list = new ListView(inflater.getContext());
 		return list;
 	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		list.setAdapter(new BaseAdapter() {
+
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public Object getItem(int position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public long getItemId(int position) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			
+			
+		});
+		
+	}
+	
+	
 	
 }
