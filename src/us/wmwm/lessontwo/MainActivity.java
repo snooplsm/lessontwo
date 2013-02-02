ca24172297514f4f792d12bdeba97d95b3d62400
Ryan Gravener <rgravener@wolf-matrix.local>
2013-02-02 12:02:07 -0500

    content fragment

--- src/us/wmwm/lessontwo/MainActivity.java
+++ src/us/wmwm/lessontwo/MainActivity.java
@@ -1,15 +1,23 @@
 package us.wmwm.lessontwo;
 
+import us.wmwm.lessontwo.fragments.ContentFragment;
+import us.wmwm.lessontwo.fragments.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
 
+	ListFragment listFragment;
+	ContentFragment contentFragment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.ListFragment);
+		contentFragment = (ContentFragment) getSupportFragmentManager().findFragmentById(R.id.ContentFragment);
 	}
 
 	@Override
---
+++ src/us/wmwm/lessontwo/fragments/ContentFragment.java
@@ -0,0 +1,29 @@
+package us.wmwm.lessontwo.fragments;
+
+import us.wmwm.lessontwo.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ContentFragment extends Fragment {
+
+	View root;
+	TextView content;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		root = inflater.inflate(R.layout.fragment_content, null);
+		content = (TextView) root.findViewById(R.id.content);
+		return root;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+}
