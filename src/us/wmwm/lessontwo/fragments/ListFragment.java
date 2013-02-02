7ba07d023ddfbf88531161a8189eff8cc3e1d48f
Ryan Gravener <rgravener@wolf-matrix.local>
2013-02-02 11:14:31 -0500

    list fragment onCreateView

--- src/us/wmwm/lessontwo/fragments/ListFragment.java
+++ src/us/wmwm/lessontwo/fragments/ListFragment.java
@@ -1,9 +1,21 @@
 package us.wmwm.lessontwo.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ListFragment extends Fragment {
 
+	ListView list;
 	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		list = new ListView(inflater.getContext());
+		return list;
+	}
 	
 }
